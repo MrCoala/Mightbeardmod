15dd43b1e81f927c0c748991c9fc3024fb00aa22
MrCoala <mrcoala@ebt08.de>
2016-11-20 13:55:20 +0100

    Initial commit

---
+++ modinfo.lua
@@ -0,0 +1,26 @@
+-- This information tells other players more about the mod
+name = "Mighty beard mod"
+description = "This mod will help you maintain your mighty beard"
+
+author = "MrCoala"
+version = "0.0.1"
+
+forumthread = ""
+
+-- This lets other players know if your mod is out of date, update it to match the current version in the game
+api_version = 10
+
+icon_atlas = ""
+icon = ""
+
+--Let the mod system know that this mod is functional with Don't Starve Together
+dst_compatible = true
+
+--Let the game know that this mod doesn't need to be listed in the server's mod listing
+client_only_mod = false
+all_clients_require_mod = false
+
+--These tags allow the server running this mod to be found with filters from the server listing screen
+server_filter_tags = {""}
+
+priority = 10
---
+++ modmain.lua
@@ -0,0 +1,73 @@
+local assert = GLOBAL.assert
+local require = GLOBAL.require
+local json = GLOBAL.json
+
+-- Get the IO library for files
+io = require "io"
+os = require "os"
+
+
+local protectedList = {
+	"firesuppressor",
+	"icebox",
+	"dragonflychest",
+	"treasurechest",
+	"cookpot",
+	"meatrack"
+}
+
+local function isProtected(prefab)
+	for i,v in ipairs(protectedList) do
+		if v == prefab then
+			return true
+		end
+	end
+	return false
+end
+
+local function printTable(tb)	
+	for k,v in pairs(tb) do
+		if type(v) == "table" then 
+			print("table: "..tostring(k))
+		else 
+			print(tostring(k)..","..tostring(v))
+		end
+	end	
+end
+
+local function writeLog(text)
+	local world_age = GLOBAL.TheWorld.components.worldstate.data.cycles
+	text = "[ALERT AT DAY: " .. world_age .. "] - " .. text
+	print( text )
+	local file = io.open("mightybeard.txt", "r")
+	if file ~= nil then
+		text = file:read("*a")
+		file:close()
+	end
+	text = text .. "\n"
+	
+	file = io.open("mightybeard.txt", "w")
+	file:write(text)
+	file:close()
+end
+
+local function say(user, say_string, time)
+	print(user.name .. ": " .. say_string)
+	user:DoTaskInTime(0, function()
+		user.components.talker:Say(say_string, time)
+	end)
+end
+
+
+local old_ACTION_HAMMER = GLOBAL.ACTIONS.HAMMER.fn
+GLOBAL.ACTIONS.HAMMER.fn = function(act)
+	
+	if act.target.prefab and isProtected(act.target.prefab) then
+		local log = "-> " .. tostring(act.doer.name) .. " hammered the " .. tostring(act.target.prefab)
+		writeLog( log )
+	end
+	
+	return old_ACTION_HAMMER(act)
+	
+end
+
